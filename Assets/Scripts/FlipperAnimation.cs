4bbf8d0f1fe7a4b54e6282e1df3d353ff69716e5
Eric Gleiser <eric@studiogleiser.com>
2018-10-13 03:30:10 -0500

    Main Board

---
+++ Assets/Scripts/FlipperAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FlipperAnimation : MonoBehaviour {
+
+    public KeyCode key = new KeyCode();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(key))
+        {
+            //transform.DOPlay();
+            //gameObject.GetComponent(DOTweenAnimation).
+
+            DOTween.PlayForward(gameObject,"flip");
+
+            print("key was pressed");
+        }
+        if (Input.GetKeyUp(key))
+        {
+            //transform.DOPlay();
+
+            DOTween.PlayBackwards(gameObject, "flip");
+            print("key was pressed");
+        }
+
+
+    }
+}
