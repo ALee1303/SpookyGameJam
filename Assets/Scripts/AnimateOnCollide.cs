62e121f5ff487cea3115ba8c172336750b6eb3af
Eric Gleiser <eric@studiogleiser.com>
2018-10-13 06:58:05 -0500

    FX

---
+++ Assets/Scripts/AnimateOnCollide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class AnimateOnCollide : MonoBehaviour {
+    [SerializeField]
+    float scoreAdd = 100;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // get voodoo doll
+        VooDooDoll doll = collision.transform.GetComponent<VooDooDoll>();
+        if (doll)
+        {
+            //ScoreManager.Instance.UpdateScore(scoreAdd);
+            DOTween.Rewind(gameObject);
+            DOTween.PlayForward(gameObject);
+        }
+    }
+}
