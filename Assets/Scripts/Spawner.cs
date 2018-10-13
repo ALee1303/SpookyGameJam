eaa61dc3a900af312cab5fb976a52ca80d631d38
Seoung_Yeon Lee <Alex.Lee@loop.colum.edu>
2018-10-13 12:45:14 -0500

    create spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour 
+{
+    [SerializeField]
+    Transform spawnPoint;
+
+    [SerializeField]
+    GameObject dollPrefab;
+
+    [SerializeField]
+    AudioClip clip;
+
+    [SerializeField]
+    float destroyDelay;
+
+    bool isDestryoing;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        VooDooDoll doll = collision.GetComponent<VooDooDoll>();
+        if (!doll)
+            return;
+        if (isDestryoing)
+            return;
+        StartCoroutine(DestroyDoll(doll));
+    }
+
+    IEnumerator DestroyDoll(VooDooDoll doll)
+    {
+        isDestryoing = true;
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(doll);
+        isDestryoing = false;
+        GameManager.Instance.OnDollDestroyed();
+        if (GameManager.Instance.GameState == GameState.Playing)
+            Instantiate(dollPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
