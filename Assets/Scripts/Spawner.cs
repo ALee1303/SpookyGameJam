633eec2d2dab8e1d2bc9608de6b5084b5bd74d04
Alex Lee <alexlee131005@gmail.com>
2018-11-05 13:18:43 -0600

    add comment to Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -24,6 +24,10 @@ public class Spawner : MonoBehaviour
 
     bool isDestryoing;
 
+    /// <summary>
+    /// Destroys the doll when the doll enters the Trigger Parameter
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         VooDooDoll doll = collision.GetComponent<VooDooDoll>();
@@ -33,20 +37,27 @@ public class Spawner : MonoBehaviour
             return;
         GetComponent<AudioSource>().Play();
         StartCoroutine(DestroyDoll());
+        // if the game didn't end from previous line, respawn the doll
+        if (GameManager.Instance.CurrentState == GameState.Playing)
+        {
+            Transform newDoll = Instantiate(dollPrefab, spawnPoint.position, Quaternion.identity).transform.GetChild(0);
+            mainCamera.target = newDoll;
+            eye.Doll = newDoll;
+        }
     }
 
+    /// <summary>
+    /// Coroutine for Destroying the doll with a delay.
+    /// Handles all the doll dependency set on GameManager after destruction
+    /// </summary>
+    /// <returns></returns>
     IEnumerator DestroyDoll()
     {
         isDestryoing = true;
         yield return new WaitForSeconds(destroyDelay);
         Destroy(GameObject.FindWithTag("Voodoo"));
         isDestryoing = false;
+        // handle GameOver logic
         GameManager.Instance.OnDollDestroyed();
-        if (GameManager.Instance.GameState == GameState.Playing)
-        {
-            Transform newDoll = Instantiate(dollPrefab, spawnPoint.position, Quaternion.identity).transform.GetChild(0);
-            mainCamera.target = newDoll;
-            eye.doll = newDoll;
-        }
     }
 }
