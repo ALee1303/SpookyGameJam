dc2e4cb39962715e9ada4aa1d0a8be0e60e8091f
Alex Lee <alexlee131005@gmail.com>
2018-11-05 06:31:52 -0600

    maake UIController absract

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour
+public abstract class UIController : MonoBehaviour
 {
     protected virtual void Awake()
     {
